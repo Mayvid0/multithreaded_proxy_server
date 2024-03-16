446071a3cc5d10ff74e5e0fc7c3a0cb6ce126471
Mayvid0 <divbillowria@gmail.com>
2024-03-17 01:42:08 +0530

    added log

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,7 +5,8 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"strings"
+	"os"
+	"time"
 )
 
 var customTransport = http.DefaultTransport
@@ -17,11 +18,11 @@ func init() {
 func handleRequest(w http.ResponseWriter, r *http.Request) {
 	// Create a new HTTP request with the same method, URL, and body as the original request
 	targetURL := r.URL
-	stringUrl := targetURL.String()
-	if strings.HasPrefix(stringUrl, "http://localhost:8080/") {
-		http.Error(w, "Can't access this URL", http.StatusBadRequest)
-		return
-	}
+	// stringUrl := targetURL.String()
+	// if strings.HasPrefix(stringUrl, "http://localhost:8080/") {
+	// 	http.Error(w, "Can't access this URL", http.StatusBadRequest)
+	// 	return
+	// }
 
 	proxyReq, err := http.NewRequest(r.Method, targetURL.String(), r.Body)
 
@@ -57,9 +58,30 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 
 	// Set the status code of the original response to the status code of the proxy response
 	w.WriteHeader(resp.StatusCode)
-
+	searchStatus := "Success"
+	if resp.StatusCode >= 400 {
+		searchStatus = "Error"
+	}
+	accessLog := fmt.Sprintf("%s - accessed by ip: %s, date: %s, status: %s\n", targetURL, r.RemoteAddr, time.Now().Format("2006-01-02 15:04:05"), searchStatus)
+	writeLogTofile(accessLog)
 	// Copy the body of the proxy response to the original response
 	io.Copy(w, resp.Body)
+}
+
+func writeLogTofile(accessLog string) {
+
+	fmt.Print("writing to log file\n")
+	f, err := os.OpenFile("logs/access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := f.Write([]byte(accessLog)); err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
