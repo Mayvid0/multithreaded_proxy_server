fad4c84b89ce6fa4235228f930747cd3e54b5bac
Mayvid0 <divbillowria@gmail.com>
2024-03-17 13:30:19 +0530

    added the test to test for concurrent writes... tested with 2000 writes ...worked fine

---
+++ test/test.go
@@ -0,0 +1,31 @@
+package test
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	logs "github.com/Mayvid0/proxy_server/internal/cacheAndLog"
+)
+
+func TestWriting() {
+
+	// Number of goroutines to simulate concurrent log writes
+	numGoroutines := 20
+
+	// Wait group to synchronize goroutines
+	var wg sync.WaitGroup
+	wg.Add(numGoroutines)
+
+	// Start goroutines to simulate concurrent log writes
+	for i := 0; i < numGoroutines; i++ {
+		go func(id int) {
+			defer wg.Done()
+			logs.WriteLogToFile(fmt.Sprintf("Log message from Goroutine %d at %s\n", id, time.Now()))
+		}(i)
+	}
+
+	// Wait for all goroutines to finish
+	wg.Wait()
+
+}
